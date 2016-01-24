bab77c94d3c53d61a0250c09403030b7e7c7262b
shubham singh <shubhmsing@gmail.com>
2016-01-24 20:15:24 +0530

    vesion 3.2

--- app/src/main/java/com/example/shubham/webbrowser/MainActivity.java
+++ app/src/main/java/com/example/shubham/webbrowser/MainActivity.java
@@ -14,6 +14,8 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.ConsoleMessage;
 import android.webkit.DownloadListener;
 import android.webkit.ValueCallback;
